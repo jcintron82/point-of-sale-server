81bafda05d21b9be286f131c4c023ccf232c21fc
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-02-28 20:25:36 -0500

    Fixed typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors({
-  origin: 'https://pos-client.onrender.com/'
+  origin: 'https://pos-client.onrender.com'
 }));
 const mongoose = require("mongoose");
 const passport = require("passport");

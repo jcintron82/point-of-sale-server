dd7761d640dae317d62b4adc07c2f3f5757f30d0
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-03-02 00:08:51 -0500

    debugging cors

--- controllers/customizedplates.js
+++ controllers/customizedplates.js
@@ -1,25 +1,50 @@
 const items = require("../DBschemas/items");
 let searchValue = '';
-const express = require("express");
-const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://pos-client.onrender.com/customizedplates"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 module.exports = {
     Post: (req, res) => {
       value = req.body[0];
       res.send({ message: value });
       searchValue = value;
       console.log(value)
+       // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
     },
     getIndex: (req, res) => {
     //   checkIfLoggedIn()
       const dbQuery = items.find({ Item: searchValue }, (err, cursor) => {
         // console.log(cursor);
-        res.json({ message: cursor });
+        res.json({ message: cursor }); // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+        next();
         cursor.splice(0);
       });
+      
     },
   };
